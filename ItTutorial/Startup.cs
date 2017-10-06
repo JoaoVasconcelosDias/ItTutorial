92a7b82a598a17fa089304e385d1188a13eb6292
Joao Dias <jpvasconcelosdias@yahoo.com>
2017-10-06 18:06:26 +0100

    way to go

--- ItTutorial/Startup.cs
+++ ItTutorial/Startup.cs
@@ -26,6 +26,24 @@ namespace ItTutorial
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = "347273735727294";
+                facebookOptions.AppSecret = "f7bc8c4528e6ab9f7aa9440474d913b5";
+            });
+
+            services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+            {
+                microsoftOptions.ClientId = "9d5f21d4 - 1d86 - 4fe0 - ab9b - 4b1622052a71";
+                microsoftOptions.ClientSecret = "ZGLNQSpucgAxcmV58Q6Pnev";
+            });
+
+            services.AddAuthentication().AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = "851406420037-se072pvhnnaakpfm239s1v5ojqs46v6s.apps.googleusercontent.com";
+                googleOptions.ClientSecret = "VJi1j5RO__y5AX0HitCi5wsc";
+            });
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<DataBaseContext>();
