1494b01e529538aabab6d92a3686ef88565e53a3
Joao Dias <jpvasconcelosdias@yahoo.com>
2017-10-12 13:01:31 +0100

    css2.2

--- ItTutorial/Controllers/HomeController.cs
+++ ItTutorial/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ItTutorial.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace ItTutorial.Controllers
 {
@@ -32,9 +34,36 @@ namespace ItTutorial.Controllers
 
         public IActionResult Certification()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your Certification page.";
 
             return View();
+        }
+
+        public IActionResult Consola()
+        {
+            ViewData["Message"] = "Your Consola page.";
+
+            return View();
+        }
+
+        //metodo
+        public class CSharpScriptEngine
+        {
+            private static ScriptState<object> scriptState = null;
+            public static object Execute(string code)
+            {
+                scriptState = scriptState == null ? CSharpScript.RunAsync(code).Result : scriptState.ContinueWithAsync(code).Result;
+                if (scriptState.ReturnValue != null && !string.IsNullOrEmpty(scriptState.ReturnValue.ToString()))
+                    return scriptState.ReturnValue;
+                return null;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Consola(string FirstCompile)
+        {           
+            object resultado = CSharpScriptEngine.Execute(FirstCompile);                              
+            return View(CSharpScriptEngine.Execute("new MyClass().Result"));                    
         }
 
         public IActionResult Error()
