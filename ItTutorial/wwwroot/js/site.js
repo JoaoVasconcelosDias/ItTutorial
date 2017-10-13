41b9815f44ab63b6653945a6a98205f6eb1b409e
Joao Dias <jpvasconcelosdias@yahoo.com>
2017-10-13 23:45:53 +0100

    1

--- ItTutorial/wwwroot/js/site.js
+++ ItTutorial/wwwroot/js/site.js
@@ -11,12 +11,3 @@
 					+'}'
 				+'}	';
 }
-
- function saveState(){
-     
-         prompt("Parabens", "Nivel", image.src);
-         image.src = https://toptrofeus.com.br/wp-content/uploads/2015/10/500212-DO-23cm-trofeu-metal-top-trofeus.jpg;
-
-
-
-}
